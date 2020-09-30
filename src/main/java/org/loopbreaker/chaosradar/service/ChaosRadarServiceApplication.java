552d8f6f65dc7daef47af29b09a9567ba1a540aa
psbateman <psbbateman@gmail.com>
2020-09-30 23:19:03 +0100

    Set-up repository #1

---
+++ src/main/java/org/loopbreaker/chaosradar/service/ChaosRadarServiceApplication.java
@@ -0,0 +1,14 @@
+package org.loopbreaker.chaosradar.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ChaosRadarServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaosRadarServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/loopbreaker/chaosradar/service/ChaosRadarServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package org.loopbreaker.chaosradar.service;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ChaosRadarServiceApplicationTest {
+    @Test
+    void contextLoads() {
+        ChaosRadarServiceApplication.main(new String[]{});
+    }
+}
