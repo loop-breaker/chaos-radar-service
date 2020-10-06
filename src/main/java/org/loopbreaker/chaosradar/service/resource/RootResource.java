6733144ebebe7b21d75cfa4b9de1b5520985bbe3
Patrick Bateman <patrick.bateman@worldpay.com>
2020-10-06 23:57:42 +0100

    Root Resource #2

--- src/main/java/org/loopbreaker/chaosradar/service/resource/RootResource.java
+++ src/main/java/org/loopbreaker/chaosradar/service/resource/RootResource.java
@@ -3,4 +3,6 @@ package org.loopbreaker.chaosradar.service.resource;
 import org.springframework.hateoas.RepresentationModel;
 
 public class RootResource extends RepresentationModel<RootResource> {
+
+
 }
